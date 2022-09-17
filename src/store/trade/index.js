d03f9c208b5a73da63bbcecd36da54f2da821175
mkcmgl <2515318247@qq.com>
2022-09-17 21:17:03 +0800

    home-trade

---
+++ src/store/trade/index.js
@@ -0,0 +1,30 @@
+import {reqAddressInfo,reqOrderInfo} from '@/api';
+const state ={
+    // feature: { property: value },
+    addressLis:[],
+};
+
+const mutations = {
+    mutationHandler(state, payload) {
+        
+    },
+};
+
+const actions = {
+    // actionHandler({ commit }, payload) {
+    //     return new Promise((resolve) => {
+    //         request().then((response) => {
+    //             commit(mutationHandler, response);
+    //             resolve(response);
+    //         });
+    //     });
+    },
+};
+const getters={};
+export default {
+    // namespaced: true,
+    state,
+    mutations,
+    actions,
+    getters,
+};
